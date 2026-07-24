df3a61c1a502ac53fb76360eaa20ef164bf31943
Foshati <amirrezafoshati@gmail.com>
2026-07-24 14:09:24 +0400

    test(cli): test admin password reset with mock database

--- crates/kuben/src/cli/admin.rs
+++ crates/kuben/src/cli/admin.rs
@@ -4,3 +4,6 @@ use kuben_api::auth::password::Hasher;
 use kuben_core::config::Config;
 
 use crate::cli::ResetAdminOpts;
+
+pub async fn reset(cfg: Config, opts: ResetAdminOpts) -> anyhow::Result<()> {
+    let store = kuben_store::Store::connect(&cfg.database).await?;
